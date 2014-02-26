acb61908aeb7f6e69b0779785a1e1fd54516a2d4
rockwasserman <ewasser@searshc.com>
2014-02-26 12:22:44 -0600

    Completed rescoring logic

--- src/main/java/metascale/bolt/ScoringBolt.java
+++ src/main/java/metascale/bolt/ScoringBolt.java
@@ -40,9 +40,9 @@ public class ScoringBolt extends BaseRichBolt {
 	private static final long serialVersionUID = 1L;
     private OutputCollector outputCollector;
     DB db;
-    DBCollection coll;
     MongoClient mongoClient;
-
+    DBCollection modelCollection;
+    DBCollection memberCollection;
 
     /*
 	 * (non-Javadoc)
@@ -62,14 +62,15 @@ public class ScoringBolt extends BaseRichBolt {
 	 */
 
         try {
-            mongoClient = new MongoClient("151.149.191.228", 27017);
+            mongoClient = new MongoClient("shrdmdb301p.stag.ch3.s.com", 20000);
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
 
-        db = mongoClient.getDB("real-time-scoring-variables");
+        db = mongoClient.getDB("RealTimeScoring");
         //db.authenticate(configuration.getString("mongo.db.user"), configuration.getString("mongo.db.password").toCharArray());
-        coll = db.getCollection("memberVariables");
+        memberCollection = db.getCollection("memberVariables");
+        modelCollection = db.getCollection("modelVariables");
     }
 
 	/*
@@ -80,106 +81,91 @@ public class ScoringBolt extends BaseRichBolt {
 	@Override
 	public void execute(Tuple input) {
 
-        JMSMessage document = (JMSMessage) input.getValueByField("npos");
+		String l_id = null;
+		JMSMessage document = (JMSMessage) input.getValueByField("npos");
 			
-			// 1 find SYWR ID for transaction
-			// 2 if SYWR ID retrieve all items in the basket
-			// 3 for each item in the basket find the division
-			// 4 if any divisions that affects the HA model - then re-score
-			// 5 store new score in mongodb
-			// 6 store the score in redis
+			// 0 Test if transaction is a sale - if not return
+			// 1 find SYWR ID for transaction OK
+			// 2 if SYWR ID retrieve all items in the basket OK
+			// 3 for each item in the basket find the division OK
+			// 4 if any divisions that affects the HA model - then re-score OK
+			// 5 store new score in mongodb  PENDING
+			// 6 store the score in redis    PENDING
 
 	        try {
+	        	
+	        	// 0 Test if transaction is a sale - if not return
+	        	
 	            String nposTransaction = ((TextMessage) document).getText();
 	            //System.out.println(nposTransaction);
 	            Collection<Segment> saleSegments = SegmentUtils.findAllSegments(nposTransaction, "B1");
-	            Integer zip = 0;
-	            char sywrCardUsed = 'N';
-	            String amount = "0";
+	            boolean isSale = false;
 	            for (Segment segment : saleSegments) {
 	                String transactionType = segment.getSegmentBody().get("Transaction Type Code");
 	                if ("1".equals(transactionType)) {
-	                    String store = segment.getSegmentBody().get("Store Number");
-	                    zip = StoreZipMap.getInstance().getZip(store);
+	                	isSale=true;
 	                }
-	                amount = segment.getSegmentBody().get("Transaction Total");
 	            }
+				
+	            if(!isSale) {
+	            	return;
+	            }
+	            
+	            // 1 find SYWR ID for transaction OK
 
 	            Collection<Segment> b2Segments = SegmentUtils.findAllSegments(nposTransaction, "B2");
 	            for (Segment segment : b2Segments) {
 	                if (segment != null && segment.getSegmentDescription() != null && segment.getSegmentDescription().contains("Type 8")) {
-	                	String l_id = segment.getSegmentBody().get("Comment Text    Craftsman Club Number or Sears Your Way Rewards");
-	                    sywrCardUsed = 'Y';
+	                	l_id = segment.getSegmentBody().get("Comment Text    Craftsman Club Number or Sears Your Way Rewards");
 	                }
 	            }
-
-	            Map changes = new HashMap();
-	            Collection<Segment> c1Segments = SegmentUtils.findAllSegments(nposTransaction, "C1");
-	            for (Segment segment : c1Segments) {
-	            	String div = segment.getSegmentBody().get("Division Number");
-	            	if(ArrayUtils.contains(new String[]{"033","041","043","045"}, div)) {
-	            		changes.put("srs_mapp_days_since_last", 0);
-	            	}
+	            
+				// 2 if SYWR ID retrieve all items in the basket OK
+	            if(l_id!=null) {
 	            	
-
-	            	
+	            
+		            Map changes = new HashMap();
+		            Collection<Segment> c1Segments = SegmentUtils.findAllSegments(nposTransaction, "C1");
+		            
+					// 3 for each item in the basket find the division OK
+		            for (Segment segment : c1Segments) {
+		            	String div = segment.getSegmentBody().get("Division Number");
+		            	if(ArrayUtils.contains(new String[]{"033","041","043","045"}, div)) {
+		            		changes.put("srs_mapp_days_since_last", 0);
+		            	}
+		            	if(ArrayUtils.contains(new String[]{"020","022","026","032","042","046"}, div)) {
+		            		changes.put("srs_appliance_days_since_last", 0);
+		            	}
+		            	if(ArrayUtils.contains(new String[]{"046"}, div)) {
+		            		changes.put("ha_refrig_0_30_purch_flg", 1);
+		            	}
+		            	
+		            	
+		            }
+		            
+					// 4 if any divisions that affects the HA model - then re-score
+		            if(!changes.isEmpty()){
+		            	//double mbrVar = calcMbrVar(changes, 1, Long.parseLong(l_id));
+		            	double newScore = 1/(1+ Math.exp(-1*(calcMbrVar(changes, 1, Long.parseLong(l_id))))) * 1000;
+		            	System.out.println(l_id + ": " + Double.toString(newScore));
+		            }
+		            else {
+		            	return;
+		            }
+		            
+		            //StringBuffer saleInfo = new StringBuffer().append(zip).append(':').append(sywrCardUsed).append(':').append(amount);
+	
+		            //if (zip != null && zip != 0)
+		                //jedis.publish("sale_info", saleInfo.toString());
 	            }
-	            
-	            if(!changes.isEmpty()){
-	            	
-	            }
-	            
-	            
-	            StringBuffer saleInfo = new StringBuffer().append(zip).append(':').append(sywrCardUsed).append(':').append(amount);
 
-	            //if (zip != null && zip != 0)
-	                //jedis.publish("sale_info", saleInfo.toString());
 	        } catch (JMSException e) {
 	            e.printStackTrace();
 	        }
 
 			
-			
-			
-        Type type = new TypeToken<Map>() {
-        }.getType();
-        Map gson =    new Gson().fromJson(document.toString(), type);
-
-        BasicDBObject query = new BasicDBObject( "LYL_ID_NO", Long.valueOf(gson.get("lyl_id_no").toString()));
-
-        DBObject member = coll.findOne(query);
-
-        System.out.println(" memeber found : " + member);
-
-
-        if (member != null) {
-        double score = 1/(1+ Math.exp(-1*(-3.4817414262759
-                + (Integer)member.get("redeemer_seg") * 0.2960757192325
-                + (Integer)member.get("srs_mapp_days_since_last") * 0.0004399620490
-                + (Integer)member.get("srs_appliance_days_since_last") * -0.0006412329322
-                + (Integer)member.get("earned_points_24m_cd") * 0.1230495072226
-                + (Integer)member.get("web_flag_refrig_0_7") * 1.5046153477196
-                + (Integer)member.get("hdln_fl") * 0.3758939603009
-                + (Double)member.get("srs_str_appliance_sales3m") * 0.0004695336403
-                + (Integer)member.get("cnt_emal_camp_12m_ind") * 0.2768082292745
-                + (Integer)member.get("srs_sears_card_amt_ind") * 0.2948286393805
-                + (Integer)member.get("HS_ANY_dsl") * -0.0000314858291
-                + (Integer)member.get("num_yr_rsd") * 0.0170213240149
-                + (Integer)member.get("HA_ANY_5y") * 0.2690159365066
-                + (Integer)member.get("sr_any_record_flg") * 1.2280939436473
-                + (Integer)member.get("ha_refrig_0_30_purch_flg") * 0.9681973501297
-                + (Integer)member.get("cnt_dm_camp_12_mth_ind") * 0.1019036215871
-                + (Integer)member.get("net_sales_24m_ind") * -0.0475925281865
-                + (Double)member.get("srs_str_mapp_sales3m") * 0.0034857097067
-                + (Double)member.get("BlackFriday_Sales_Index_Srs") * -0.0021117730335
-                + (Integer)member.get("gift_giver_srs") * -3.2426464544210
-                + (Integer)member.get("HA_ANY_indi") * 0.1390558536262))) * 1000;
-
-            System.out.println(" new score!!! : " + score);
-
 
         }
-	}
 
     /*
       * (non-Javadoc)
@@ -190,9 +176,10 @@ public class ScoringBolt extends BaseRichBolt {
       */
 	@Override
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		
 	}
 
-    private static double calcMbrVar( Map changes, String modelId, DBCollection modelCollection, long LID, DBCollection memberCollection)
+    private double calcMbrVar( Map changes, int modelId, long LID)
     {
 	    
         BasicDBObject queryModel = new BasicDBObject("modelId", modelId);
@@ -229,8 +216,8 @@ public class ScoringBolt extends BaseRichBolt {
 		    + var.getStrategy()   + ", " 
 		    + var.getCoefficeint() );
 		    
-		    if(  var.getType().equals("Integer")) val = val + ((Integer)member.get(var.getVid()) * var.getCoefficeint());
-		    else if( var.getType().equals("Double")) val = val + ((Double)member.get(var.getVid()) * var.getCoefficeint());
+		    if(  var.getType().equals("Integer")) val = val + ((Integer)calculateVariableValue(member, var, changes, var.getType()) * var.getCoefficeint());
+		    else if( var.getType().equals("Double")) val = val + ((Double)calculateVariableValue(member, var, changes, var.getType()) * var.getCoefficeint());
 		    else {
 		    	val = 0;
 		    	break;
@@ -265,6 +252,23 @@ public class ScoringBolt extends BaseRichBolt {
     */
 
     }
+
+	private Object calculateVariableValue(DBObject member, Variable var, Map changes, String dataType) {
+		Object changedVar = changes.get(var.getName());
+		if(changedVar == null) {
+			changedVar=member.get(var.getVid());
+		}
+		else{
+			if(dataType.equals("Integer")) {
+				changedVar=Integer.parseInt(changedVar.toString());
+			}
+			else {
+				changedVar=Double.parseDouble(changedVar.toString());
+			}
+		}
+		System.out.println("changed variable value: " + changedVar.toString());
+		return changedVar;
+	}
 
 	
 }
