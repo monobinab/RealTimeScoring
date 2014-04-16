78837a41827b43811653cae9f7d196523e9dd5fe
Sanjay Yermalkar <syermalk@searshc.com>
2014-04-16 15:03:04 -0500

    Added Redis listening support.

---
+++ src/main/java/analytics/BrightTagsTopology.java
@@ -0,0 +1,44 @@
+package analytics;
+
+import analytics.bolt.PrinterBolt;
+import analytics.spout.RedisPubSubSpout;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.topology.TopologyBuilder;
+
+/**
+ * This topology demonstrates Storm's stream groupings and multilang capabilities.
+ */
+public class BrightTagsTopology {
+
+
+  public static void main(String[] args) throws Exception {
+
+    TopologyBuilder builder = new TopologyBuilder();
+
+    builder.setSpout("spout", new RedisPubSubSpout("rtsapp302p.qa.ch3.s.com", 6379, "bright_tags"), 1);
+
+    builder.setBolt("print", new PrinterBolt(), 1).shuffleGrouping("spout");
+    //builder.setBolt("print", new RealtyTracBolt(), 2).shuffleGrouping("spout").shuffleGrouping("spout2");
+
+
+    Config conf = new Config();
+
+
+
+    if (args != null && args.length > 0) {
+      conf.setNumWorkers(3);
+
+      StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+    }
+    else {
+        conf.setDebug(true);
+      conf.setMaxTaskParallelism(3);
+      LocalCluster cluster = new LocalCluster();
+      cluster.submitTopology("brightTags", conf, builder.createTopology());
+      Thread.sleep(10000000);
+      cluster.shutdown();
+    }
+  }
+}
