48ee7422af73d57cc52e17021c7cdc1bbe3d31ed
rockwasserman <ewasser@searshc.com>
2014-07-29 18:52:48 -0500

    completed StrategyCountTraitDates - not tested

--- src/main/java/analytics/util/strategies/StrategyCountTraitDates.java
+++ src/main/java/analytics/util/strategies/StrategyCountTraitDates.java
@@ -1,5 +1,66 @@
 package analytics.util.strategies;
 
-public class StrategyCountTraitDates {
+import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.joda.time.LocalDate;
+
+import analytics.util.Change;
+import analytics.util.RealTimeScoringContext;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
+public class StrategyCountTraitDates implements Strategy {
+	private int daysToExpiration = 1;
+	
+
+    @Override
+    public Change execute(RealTimeScoringContext context) {
+		
+    	Map<String,Collection<String>> dateTraitsMap = restoreDateTraitsMapFromJson((String) context.getValue());
+    	SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    	int dateCount = 0;
+    			
+    	//FOR EACH TRAIT FOUND FROM AAM DATA FIND THE VARIABLES THAT ARE IMPACTED
+    	for(String date: dateTraitsMap.keySet()) {
+    		try {
+				if(simpleDateFormat.parse(date).after(new Date(new Date().getTime() + (-7 * 1000 * 60 * 60 * 24)))) {
+					if(!dateTraitsMap.get(date).isEmpty()) {
+						dateCount++;
+					}
+				}
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+    	}
+    	
+    	return new Change(dateCount, calculateExpirationDate());
+    }
+
+	public static Map<String, Collection<String>> restoreDateTraitsMapFromJson(String json)
+    {
+		Map<String, Collection<String>> dateTraitsMap = new HashMap<String, Collection<String>>();
+        Type dateTraitType = new TypeToken<Map<String, Collection<String>>>() {
+			private static final long serialVersionUID = 1L;
+		}.getType();
+
+		dateTraitsMap = new Gson().fromJson(json, dateTraitType);
+//        System.out.println(" JSON string: " + json);
+//        System.out.println(" Map: " + dateTraitsMap);
+        return dateTraitsMap;
+    }
+
+    private Date calculateExpirationDate() {
+		return new LocalDate(new Date()).plusDays(this.daysToExpiration).toDateMidnight().toDate();
+	}
+
 
 }
