b4baafa63b246e3a8444c41959e16fcafdbdab78
Devika Nair <Devika.NairSyama@searshc.com>
2014-09-10 09:53:29 -0500

    Testing bolts

---
+++ src/test/java/analytics/FBBoltMockTest.java
@@ -0,0 +1,35 @@
+package analytics;
+
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import backtype.storm.tuple.Tuple;
+import analytics.bolt.FacebookBolt;
+
+public class FBBoltMockTest {
+	@Test
+	public void basicFBTest() {
+		MockOutputCollector outputCollector = new MockOutputCollector(null);
+        FacebookBolt boltUnderTest = new FacebookBolt();
+       
+        boltUnderTest.prepare(null, null, outputCollector);
+        String input = "8/1/2014 7:07,1123404212,[0.0],[0.0],dishwasher";
+        String expectedLid = "y2gpsDmSmaKudbyxsGUbpDeTU1Q=";
+        String expectedBoostVar = "{\"BOOST_DISHWASHER_FB\":\"0.0\"}";//postive score is only considered here
+        String source = "FB";
+        //source: facebookSpout:3, stream: default, id: {}, [8/1/2014 7:07,1123404212,[0.0],[0.0],dishwasher]
+        Tuple tuple = StormTestUtils.mockTuple(input);
+        boltUnderTest.execute(tuple);
+        
+        List<Object> outputTuple = outputCollector.getTuple();
+        
+        Assert.assertEquals(expectedLid, outputTuple.get(0));
+        Assert.assertEquals(expectedBoostVar, outputTuple.get(1));
+        Assert.assertEquals(source, outputTuple.get(2));
+        //[y2gpsDmSmaKudbyxsGUbpDeTU1Q=, {"BOOST_DISHWASHER_FB":"0."}, FB]
+	}
+
+}
