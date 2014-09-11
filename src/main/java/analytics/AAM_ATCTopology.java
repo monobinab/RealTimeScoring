800e213534d015011dc998c67f953352d08bbf4a
Devika Nair <Devika.NairSyama@searshc.com>
2014-09-11 10:20:00 -0500

    organize imports

--- src/main/java/analytics/AAM_ATCTopology.java
+++ src/main/java/analytics/AAM_ATCTopology.java
@@ -1,8 +1,5 @@
 package analytics;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.commons.configuration.ConfigurationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
