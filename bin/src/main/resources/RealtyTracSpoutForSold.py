6fca3d087c6f3602c32be952c58bab82e6704b49
Devika Nair <Devika.NairSyama@searshc.com>
2014-09-17 10:12:22 -0500

    rmeoving files

--- bin/src/main/resources/RealtyTracSpoutForSold.py
+++
@@ -1,3 +0,0 @@
-import realtytrac
-
-realtytrac.RealtyTracSpoutForSold().run()
--- bin/src/main/resources/run.sh
+++
@@ -1,6 +0,0 @@
-# HC's
-ZWSID="X1-ZWz1bctzwsojkb_3v2ae"
-
-python ./Get_Property_Details.py $ZWSID "adrs.csv" 8000 8005
-
-# The End
