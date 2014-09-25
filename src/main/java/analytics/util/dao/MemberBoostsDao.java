a211a03103f267ead066cd065d9257f9e2b4cf60
Devika Nair <Devika.NairSyama@searshc.com>
2014-09-25 13:43:11 -0500

    MemberBoostDao

--- src/main/java/analytics/util/dao/MemberBoostsDao.java
+++ src/main/java/analytics/util/dao/MemberBoostsDao.java
@@ -3,6 +3,7 @@ package analytics.util.dao;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -18,42 +19,59 @@ import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 
 public class MemberBoostsDao extends AbstractDao {
+	public static void main(String[] args) {
+		MemberBoostsDao dao = new MemberBoostsDao();
+		Set<String> boostSet = new HashSet<String>();
+		boostSet.add("BOOST_SYW_CE_LIKETCOUNT");
+		boostSet.add("BOOST_SYW_XX_LIKETCOUNT");
+		boostSet.add("BOOST_SYW_HA_LIKETCOUNT");
+		dao.getMemberBoostsValues("dxo0b7SN1eER9shCSj0DX+eSGag=", boostSet);
+	}
+
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(MemberVariablesDao.class);
-	
-    DBCollection memberBoostsCollection;
-    
-    public MemberBoostsDao(){
-    	super();
-    	memberBoostsCollection = db.getCollection(MongoNameConstants.MBR_BOOSTS_COLLECTION);
-    }
+
+	DBCollection memberBoostsCollection;
+
+	public MemberBoostsDao() {
+		super();
+		memberBoostsCollection = db
+				.getCollection(MongoNameConstants.MBR_BOOSTS_COLLECTION);
+	}
 
 	public Map<String, Map<String, List<String>>> getMemberBoostsValues(
 			String l_id, Set<String> boostSet) {
-		
+
 		// Map of boost name to a map of date to a list of values
 		Map<String, Map<String, List<String>>> mapToReturn = new HashMap<String, Map<String, List<String>>>();
-		
-		DBObject mbrBoostsDBO = memberBoostsCollection.findOne(new BasicDBObject(MongoNameConstants.L_ID, l_id));
-		if(mbrBoostsDBO == null) {
+
+		DBObject mbrBoostsDBO = memberBoostsCollection
+				.findOne(new BasicDBObject(MongoNameConstants.L_ID, l_id));
+		if (mbrBoostsDBO == null) {
 			return null;
 		}
-		BasicDBList mbrBoostsList = (BasicDBList) mbrBoostsDBO.get(MongoNameConstants.BOOSTS_ARRAY);
-		if(mbrBoostsList == null) {
+		BasicDBList mbrBoostsList = (BasicDBList) mbrBoostsDBO
+				.get(MongoNameConstants.BOOSTS_ARRAY);
+		if (mbrBoostsList == null) {
 			return null;
 		}
-		
-		for(String boost: mbrBoostsList.keySet()) {
-			if(!mapToReturn.containsKey(boost) && boostSet.contains(boost)) {
-				mapToReturn.put(boost, new HashMap<String, List<String>>());
-				BasicDBList dateDBList = (BasicDBList) mbrBoostsList.get(boost);
-				for(String date: dateDBList.keySet()) {
-					if(!mapToReturn.get(boost).containsKey(date)) {
-						mapToReturn.get(boost).put(date, new ArrayList<String>());
-					}
-					BasicDBList valuesDBList = (BasicDBList) dateDBList.get(date);
-					for(String value: valuesDBList.keySet()) {
-						mapToReturn.get(boost).get(date).add(value);
+
+		for (Object boostObj : mbrBoostsList) {
+			for (String boost : ((DBObject) boostObj).keySet()) {
+				if (!mapToReturn.containsKey(boost) && boostSet.contains(boost)) {
+					mapToReturn.put((String) boost,	new HashMap<String, List<String>>());
+					BasicDBList dateDBList = (BasicDBList) ((DBObject) boostObj)
+							.get(boost);
+					for (Object dateValue : dateDBList) {
+						for (String date : ((DBObject) dateValue).keySet()) {
+							if (!mapToReturn.get(boost).containsKey(date)) {
+								mapToReturn.get(boost).put((String) date,new ArrayList<String>());
+							}
+							BasicDBList valuesDBList = (BasicDBList) ((DBObject)dateValue).get(date);
+							for (Object value : valuesDBList) {
+								mapToReturn.get(boost).get(date).add(value.toString());
+							}
+						}
 					}
 				}
 			}
