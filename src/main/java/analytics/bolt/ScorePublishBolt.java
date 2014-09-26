c45c4f6ec808ea4f79b1374154cf02834df3544f
Devika Nair <Devika.NairSyama@searshc.com>
2014-09-26 10:58:54 -0500

    Score publish retry

--- src/main/java/analytics/bolt/ScorePublishBolt.java
+++ src/main/java/analytics/bolt/ScorePublishBolt.java
@@ -25,32 +25,33 @@ public class ScorePublishBolt extends BaseRichBolt {
 	 *
 	 */
 	private static final long serialVersionUID = 1L;
-    private OutputCollector outputCollector;
-    final String host;
-    final int port;
-    final String pattern;
+	private OutputCollector outputCollector;
+	final String host;
+	final int port;
+	final String pattern;
 
-    private Jedis jedis;
-    private MemberScoreDao memberScoreDao;
+	private Jedis jedis;
+	private MemberScoreDao memberScoreDao;
 
-    public ScorePublishBolt(String host, int port, String pattern) {
-        this.host = host;
-        this.port = port;
-        this.pattern = pattern;
-    }
+	public ScorePublishBolt(String host, int port, String pattern) {
+		this.host = host;
+		this.port = port;
+		this.pattern = pattern;
+	}
 
-    /*
-         * (non-Javadoc)
-         *
-         * @see backtype.storm.task.IBolt#prepare(java.util.Map,
-         * backtype.storm.task.TopologyContext, backtype.storm.task.OutputCollector)
-         */
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see backtype.storm.task.IBolt#prepare(java.util.Map,
+	 * backtype.storm.task.TopologyContext, backtype.storm.task.OutputCollector)
+	 */
 	@Override
-	public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
-        jedis = new Jedis(host, port);
-        memberScoreDao = new MemberScoreDao();
-        this.outputCollector = collector;
-    }
+	public void prepare(Map stormConf, TopologyContext context,
+			OutputCollector collector) {
+		jedis = new Jedis(host, port);
+		memberScoreDao = new MemberScoreDao();
+		this.outputCollector = collector;
+	}
 
 	/*
 	 * (non-Javadoc)
@@ -59,46 +60,47 @@ public class ScorePublishBolt extends BaseRichBolt {
 	 */
 	@Override
 	public void execute(Tuple input) {
-		LOGGER.debug("The time it enters inside Score Publish Bolt execute method "+System.currentTimeMillis());
-        //System.out.println(" %%% scorepublishbolt :" + input);
-        String l_id = input.getStringByField("l_id");
-        String modelId = input.getStringByField("model");
-        String modelName = ScoringSingleton.getInstance().getModelName(Integer.parseInt(modelId));
-        String oldScore = memberScoreDao.getMemberScores(l_id).get(modelId);
-        String message = new StringBuffer(l_id).append(",")
-            .append(modelName).append(",")
-            .append(input.getStringByField("source")).append(",")
-            .append(input.getDoubleByField("newScore")).append(",")
-            .append(oldScore).toString();
-        //System.out.println(" %%% message : " + message);       
-        
-        int retryCount=0;
-        while(retryCount<5)
-        try{
-        	jedis.publish(pattern, message);
-        	break;
-        }
-        catch(Exception e){
-        	LOGGER.error(e.getMessage(),e);
-        	try {
-				Thread.sleep(200);
-			} catch (InterruptedException e1) {
-	        	LOGGER.error(e.getMessage(),e);
-	        	outputCollector.fail(input);
+		LOGGER.debug("The time it enters inside Score Publish Bolt execute method "
+				+ System.currentTimeMillis());
+		// System.out.println(" %%% scorepublishbolt :" + input);
+		String l_id = input.getStringByField("l_id");
+		String modelId = input.getStringByField("model");
+		String modelName = ScoringSingleton.getInstance().getModelName(
+				Integer.parseInt(modelId));
+		String oldScore = memberScoreDao.getMemberScores(l_id).get(modelId);
+		String message = new StringBuffer(l_id).append(",").append(modelName)
+				.append(",").append(input.getStringByField("source"))
+				.append(",").append(input.getDoubleByField("newScore"))
+				.append(",").append(oldScore).toString();
+		// System.out.println(" %%% message : " + message);
+
+		int retryCount = 0;
+		while (retryCount < 5) {
+			try {
+				jedis.publish(pattern, message);
+				break;
+			} catch (Exception e) {
+				LOGGER.error(e.getMessage(), e);
+				try {
+					Thread.sleep(200);
+				} catch (InterruptedException e1) {
+					LOGGER.error(e.getMessage(), e);
+					if(retryCount==4)
+						outputCollector.fail(input);
+				}
+				retryCount++;
 			}
-        	jedis.publish(pattern, message);
-        	retryCount++;
-        }
-        outputCollector.ack(input);
+		}
+		outputCollector.ack(input);
 	}
 
-    /*
-      * (non-Javadoc)
-      *
-      * @see
-      * backtype.storm.topology.IComponent#declareOutputFields(backtype.storm.
-      * topology.OutputFieldsDeclarer)
-      */
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * backtype.storm.topology.IComponent#declareOutputFields(backtype.storm.
+	 * topology.OutputFieldsDeclarer)
+	 */
 	@Override
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
 	}
