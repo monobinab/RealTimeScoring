31001d9ebdf917faa14f7e276fdb4a5658b35f71
Devika Nair <Devika.NairSyama@searshc.com>
2014-10-20 10:38:40 -0500

    Pass topologyname in metrics listener

--- src/main/java/analytics/util/MetricsListener.java
+++ src/main/java/analytics/util/MetricsListener.java
@@ -10,23 +10,26 @@ import backtype.storm.task.TopologyContext;
 
 public class MetricsListener implements IMetricsConsumer {
 
+	String topologyName;
 	Jedis jedis;
+	
 	@Override
 	public void prepare(Map stormConf, Object registrationArgument,
 			TopologyContext context, IErrorReporter errorReporter) {
 		jedis = new Jedis("10.2.8.175", 11211);
-
+		topologyName = (String) stormConf.get("topology.name");
 	}
 
 	@Override
 	public void handleDataPoints(TaskInfo taskInfo,
 			Collection<DataPoint> dataPoints) {
-		String compId = taskInfo.srcWorkerHost  + ":" + taskInfo.srcWorkerPort + " - " + taskInfo.srcComponentId + " - " + taskInfo.timestamp + " : ";
+		String compId = new StringBuilder().append(topologyName).append("|").append(taskInfo.srcWorkerHost).append(":")
+				.append(taskInfo.srcWorkerPort).append("|").append(taskInfo.srcComponentId).append("|").append(taskInfo.timestamp).append("|").toString();
 		for (DataPoint dataPoint : dataPoints) {
 			if (dataPoint.name.equalsIgnoreCase("custom_metrics")) {
 				Map<String, Object> map = (Map<String, Object>) dataPoint.value;
 				for (String key : map.keySet()) {
-					jedis.publish("metrics", compId + key + " - " + map.get(key));
+					jedis.publish("metrics", compId + key + "|" + map.get(key));
 				}
 				}
 		}
