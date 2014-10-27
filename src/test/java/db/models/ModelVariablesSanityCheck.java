6767b080d44e7b8f63a017e667a7961c464b680a
Devika Nair <Devika.NairSyama@searshc.com>
2014-10-27 10:28:23 -0500

    adding db model test

---
+++ src/test/java/db/models/ModelVariablesSanityCheck.java
@@ -0,0 +1,34 @@
+package db.models;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ModelVariablesSanityCheck{
+	@Test
+	public void checkModelVariablesJsonFile() throws FileNotFoundException, IOException, ParseException{
+		JSONParser parser = new JSONParser();
+
+		String content = new String(Files.readAllBytes(Paths.get("db/models/modelVariables.json")), Charset.defaultCharset());
+		Object obj = parser.parse("["+content+"]");
+
+		JSONArray modelsArray = (JSONArray) obj;
+		for(Object model: modelsArray){
+			JSONObject modelObject = (JSONObject)model;
+			Assert.assertTrue((Long)modelObject.get("modelId")>0);
+			Assert.assertTrue((Long)modelObject.get("modelId")<76);
+			String modelName = (String)modelObject.get("modelName");
+			Assert.assertTrue(modelName.startsWith("S_SCR_")||modelName.startsWith("K_SCR_"));
+		}
+	}
+}
