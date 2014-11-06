c67fccca645de4a6ab2131de74ed50dfd3dc1bba
Devika Nair <Devika.NairSyama@searshc.com>
2014-11-06 17:46:39 -0600

    improved pid matching

--- src/main/java/analytics/util/PidMatchUtils.java
+++ src/main/java/analytics/util/PidMatchUtils.java
@@ -23,14 +23,19 @@ public class PidMatchUtils{
 			//market place item -  we can not process it
 		}
 		//If pid does not end with P, we need to add it
-		if(!(pid.endsWith("P")||pid.endsWith("B"))){
+		if((pid.length()==11||pid.length()==16) && !pid.endsWith("P")){
 			pid = pid+'P';
 		}
+		DivLn divLnObj = pidDivLn.getDivLnFromPid(pid);
+		if(divLnObj!=null){
+			return divLnObj;
+		}
+		
 		if(pid.length()==12){
 			//If PID length equals 12, then it is a Sears product and div and itm_no can be parsed out
-			div = pid.substring(0, 2);
-			item = pid.substring(3, 7);
-			divLn = divLnItm.getLnFromDivItem(div, item);
+			div = pid.substring(0, 3);
+			item = pid.substring(3, 8);
+			divLn = div + divLnItm.getLnFromDivItem(div, item);
 			return new DivLn(div, divLn); 
 		}
 		if(pid.length()==14 && pid.contains("CO")){
@@ -41,11 +46,10 @@ public class PidMatchUtils{
 			div= pid.substring(0,2);
 			if(pid.contains("V")||pid.contains("W")){
 				item = pid.substring(4,12);//kmart item
-				divLn = divLnItm.getLnFromDivItem(div, item);
+				divLn = div + divLnItm.getLnFromDivItem(div, item);
 				return new DivLn(div, divLn);
 			}
 		}
-		//Default lookup. Also length = 14 and contains VA comes here
-		return pidDivLn.getDivLnFromPid(pid);
+		return null;
 	}
 }
