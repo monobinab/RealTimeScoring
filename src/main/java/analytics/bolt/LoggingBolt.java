0ca91d5316227c555f62a7ca6b66e6ca0ba0fd8f
dnairsy <dnairsy@searshc.com>
2015-01-30 14:05:12 -0600

    Add logging bolt

--- src/main/java/analytics/bolt/LoggingBolt.java
+++ src/main/java/analytics/bolt/LoggingBolt.java
@@ -23,26 +23,19 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ScorePublishBolt extends BaseRichBolt {
+public class LoggingBolt extends BaseRichBolt {
 
 	private static final Logger LOGGER = LoggerFactory
-			.getLogger(ScorePublishBolt.class);
+			.getLogger(LoggingBolt.class);
 	/**
 	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	private OutputCollector outputCollector;
-	final String host;
-	final int port;
-	final String pattern;
 
-	private JedisPool jedisPool;
 	private MemberScoreDao memberScoreDao;
 	private MultiCountMetric countMetric;
-	public ScorePublishBolt(String host, int port, String pattern) {
-		this.host = host;
-		this.port = port;
-		this.pattern = pattern;
+	public LoggingBolt() {
 	}
 
 	/*
@@ -54,12 +47,7 @@ public class ScorePublishBolt extends BaseRichBolt {
 	@Override
 	public void prepare(Map stormConf, TopologyContext context,
 			OutputCollector collector) {
-        System.setProperty(MongoNameConstants.IS_PROD, String.valueOf(stormConf.get(MongoNameConstants.IS_PROD)));
-	     initMetrics(context);
-        JedisPoolConfig poolConfig = new JedisPoolConfig();
-        poolConfig.setMaxActive(100);
-        jedisPool = new JedisPool(poolConfig,host, port, 100);
-//		jedis = new Jedis(host, port);
+	    initMetrics(context);
 		memberScoreDao = new MemberScoreDao();
 		this.outputCollector = collector;
 	}
@@ -79,44 +67,18 @@ public class ScorePublishBolt extends BaseRichBolt {
 		// System.out.println(" %%% scorepublishbolt :" + input);
 		String l_id = input.getStringByField("l_id");
 		String modelId = input.getStringByField("model");
-		String modelName = ScoringSingleton.getInstance().getModelName(
-				Integer.parseInt(modelId));
 		String oldScore = memberScoreDao.getMemberScores(l_id).get(modelId);
 		String source = input.getStringByField("source");
 		Double newScore = input.getDoubleByField("newScore");
-		String message = new StringBuffer(l_id).append(",").append(modelName)
-				.append(",").append(source)
-				.append(",").append(newScore)
-				.append(",").append(oldScore).toString();
-		// System.out.println(" %%% message : " + message);
+		String minExpiry = input.getStringByField("minExpiry");
 		String messageID = "";
 		if (input.contains("messageID")) {
 			messageID = input.getStringByField("messageID");
 		}
-		LOGGER.info("TIME:" + messageID + "-Entering score publish bolt-" + System.currentTimeMillis());
-		int retryCount = 0;
-		while (retryCount < 5) {
-			try {
-				Jedis jedis = jedisPool.getResource();
-				jedis.publish(pattern, message);
-				jedisPool.returnResource(jedis);
-				break;
-			} catch (Exception e) {
-				LOGGER.error(e.getMessage(), e);
-				try {
-					Thread.sleep(200);
-				} catch (InterruptedException e1) {
-					LOGGER.error(e.getMessage(), e);
-					if(retryCount==4)
-						outputCollector.fail(input);
-				}
-				retryCount++;
-			}
-		}
-		countMetric.scope("publish_successful").incr();
-		outputCollector.ack(input);
-		LOGGER.info("TIME:" + messageID + "-Score publish complete-" + System.currentTimeMillis());
-		}
+		LOGGER.info("TIME:" + messageID + "-Entering logging bolt-" + System.currentTimeMillis());
+		LOGGER.info("PERSIST: " + new Date() + ": Topology: Changes Scores : lid: " + l_id + ", modelId: "+modelId + ", oldScore: "+oldScore +", newScore: "+newScore+", minExpiry: "+minExpiry+": source: " + source);
+		countMetric.scope("score_logged").incr();
+		outputCollector.ack(input);	}
 
 	/*
 	 * (non-Javadoc)
