72fcb569008161585757453faa7d4f6395d7d4ac
Karththikka Muthukuri <kmuthuk@SYW00490GU13053.kih.kmart.com>
2015-03-30 09:39:34 -0500

    loggingBolt added in RealTimeScoring topology

--- src/main/java/analytics/bolt/StrategyScoringBolt.java
+++ src/main/java/analytics/bolt/StrategyScoringBolt.java
@@ -70,8 +70,8 @@ public class StrategyScoringBolt extends EnvironmentBolt {
 	@Override
 	public void prepare(Map stormConf, TopologyContext context,
 			OutputCollector collector) {
-		super.prepare(stormConf, context, collector);
-	//	System.setProperty(MongoNameConstants.IS_PROD, environment);
+	//	super.prepare(stormConf, context, collector);
+		System.setProperty(MongoNameConstants.IS_PROD, environment);
 		LOGGER.info("PREPARING STRATEGY SCORING BOLT");	
 	     initMetrics(context);
 	  	this.outputCollector = collector;
