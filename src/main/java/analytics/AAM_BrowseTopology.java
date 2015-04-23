4a8d72feb862e3810265ae81f7ededb524acd045
spannal <spannal@ushoflt323507.kih.kmart.com>
2015-04-23 11:50:33 -0500

    browse topology update

--- src/main/java/analytics/AAM_BrowseTopology.java
+++ src/main/java/analytics/AAM_BrowseTopology.java
@@ -45,13 +45,13 @@ public class AAM_BrowseTopology {
 	  		.shuffleGrouping("browseSpout");
 
 		
-		/*topologyBuilder.setBolt("strategyScoringBolt", new StrategyScoringBolt(System
+		topologyBuilder.setBolt("strategyScoringBolt", new StrategyScoringBolt(System
 				.getProperty(MongoNameConstants.IS_PROD)), 3)
 				.localOrShuffleGrouping("parsingBoltBrowse");
 		
 		if(System.getProperty(MongoNameConstants.IS_PROD).equals("PROD")){
 			topologyBuilder.setBolt("flumeLoggingBolt", new FlumeRPCBolt(System.getProperty(MongoNameConstants.IS_PROD)), 1).shuffleGrouping("strategyScoringBolt", "score_stream");
-		}*/
+		}
 		
 //		 topologyBuilder.setBolt("scorePublishBolt", new ScorePublishBolt(RedisConnection.getServers()[0], 6379,"score"), 3).localOrShuffleGrouping("strategyScoringBolt", "score_stream");
 	//        topologyBuilder.setBolt("memberPublishBolt", new MemberPublishBolt(RedisConnection.getServers()[0], 6379,"member"), 3).localOrShuffleGrouping("strategyScoringBolt", "member_stream");
