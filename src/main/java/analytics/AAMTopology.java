03fe6f5fb5129c354904d5bd1714298f65bda253
Karththikka Muthukuri <kmuthuk@SYW00490GU13053.kih.kmart.com>
2015-04-27 12:16:47 -0500

    common functions incorporated for AAMTraits topology

--- src/main/java/analytics/AAMTopology.java
+++ src/main/java/analytics/AAMTopology.java
@@ -35,41 +35,26 @@ public class AAMTopology {
 			System.out
 					.println("Please pass the environment variable argument- 'PROD' or 'QA' or 'LOCAL'");
 			System.exit(0);
-		} else {
-		String topic = TopicConstants.AAM_CDF_TRAITS; 
-		int port = TopicConstants.PORT;
-		TopologyBuilder builder = new TopologyBuilder();
+		}
+			TopologyBuilder builder = new TopologyBuilder();
 
 	   	String[] servers = RedisConnection.getServers(System.getProperty(MongoNameConstants.IS_PROD));
-	   	//Sree. Commented to disable the spouts since we have the webhdfs. 
-	    /*builder.setSpout("AAM_CDF_Traits1", new AAMRedisPubSubSpout(servers[0], port, topic), 1);
-	    builder.setSpout("AAM_CDF_Traits2", new AAMRedisPubSubSpout(servers[1], port, topic), 1);
-	    builder.setSpout("AAM_CDF_Traits3", new AAMRedisPubSubSpout(servers[2], port, topic), 1);*/
-	   	
+	  
 	   	//Sree. Spout that wakes up every 5 mins and process the Traits
 	  	builder.setSpout("traitsSpout", new WebHDFSSpout(servers[1], TopicConstants.PORT, Constants.AAM_TRAITS_PATH, "aamTraits"), 1);
 	  	builder.setBolt("parsingBoltWebTraits", new ParsingBoltWebTraits(System.getProperty(MongoNameConstants.IS_PROD), "aamTraits"), 1)
 	  		.shuffleGrouping("traitsSpout");
 	  	builder.setBolt("strategyScoringBolt", new StrategyScoringBolt(System.getProperty(MongoNameConstants.IS_PROD)),1).shuffleGrouping("parsingBoltWebTraits");
 	    builder.setBolt("persistTraits" , new PersistTraitsBolt(), 1).shuffleGrouping("parsingBoltWebTraits");
+	    if(System.getProperty(MongoNameConstants.IS_PROD).equalsIgnoreCase("PROD")){
 	    builder.setBolt("flumeLoggingBolt", new FlumeRPCBolt(System.getProperty(MongoNameConstants.IS_PROD)), 1).shuffleGrouping("strategyScoringBolt", "score_stream");
+	    }
 
-	    //builder.setBolt("parsingBoltWebTraits", new ParsingBoltWebTraits(), 1)
-	    //.shuffleGrouping("AAM_CDF_Traits1").shuffleGrouping("AAM_CDF_Traits2").shuffleGrouping("AAM_CDF_Traits3");
-	    /*builder.setBolt("strategyScoringBolt", new StrategyScoringBolt(),1).shuffleGrouping("parsingBoltWebTraits");
-	    builder.setBolt("persistTraits" , new PersistTraitsBolt(), 1).shuffleGrouping("parsingBoltWebTraits");
-	    builder.setBolt("flumeLoggingBolt", new FlumeRPCBolt(), 1).shuffleGrouping("strategyScoringBolt", "score_stream");*/
-		
-	    //builder.setBolt("scorePublishBolt", new ScorePublishBolt(servers[0], port,"score"), 1).shuffleGrouping("strategyScoringBolt", "score_stream");
-	    //builder.setBolt("memberPublishBolt", new MemberPublishBolt(RedisConnection.getServers()[0], 6379,"member"), 2).shuffleGrouping("strategyScoringBolt", "member_stream");
-	      
 	    Config conf = new Config();
 		conf.put("metrics_topology", "AamTraits");
-	    conf.registerMetricsConsumer(MetricsListener.class, 3);
+	    conf.registerMetricsConsumer(MetricsListener.class, System.getProperty(MongoNameConstants.IS_PROD), 3);
 		conf.setDebug(false);
 		conf.setNumWorkers(3);
-		//stormconf is set with system's property as MetricsListener needs it
-		conf.put("topology_environment", System.getProperty(MongoNameConstants.IS_PROD));
 		if (System.getProperty(MongoNameConstants.IS_PROD)
 				.equalsIgnoreCase("PROD")
 				|| System.getProperty(MongoNameConstants.IS_PROD)
@@ -97,6 +82,6 @@ public class AAMTopology {
 
 		}
 	
-		}
+		
 		}
 }
