07afb71cbca0e156c6338c3181ccf7a4550e9050
Karththikka Muthukuri <kmuthuk@SYW00490GU13053.kih.kmart.com>
2015-05-18 15:26:59 -0500

    responsys bolt increased to 12 in telluride

--- src/main/java/analytics/RealTimeScoringTellurideTopology.java
+++ src/main/java/analytics/RealTimeScoringTellurideTopology.java
@@ -80,7 +80,7 @@ public class RealTimeScoringTellurideTopology {
        
         topologyBuilder.setBolt("responsysBolt", new ResponsysUnknownCallsBolt(System.getProperty(MongoNameConstants.IS_PROD), AuthPropertiesReader
 				.getProperty(Constants.RESPONSE_REDIS_SERVER_HOST), new Integer (AuthPropertiesReader
-				.getProperty(Constants.RESPONSE_REDIS_SERVER_PORT))), 3).shuffleGrouping("strategyScoringBolt", "response_stream");
+				.getProperty(Constants.RESPONSE_REDIS_SERVER_PORT))), 12).shuffleGrouping("strategyScoringBolt", "response_stream");
 		//Redis publish to server 1
         //topologyBuilder.setBolt("scorePublishBolt", new ScorePublishBolt(RedisConnection.getServers()[0], 6379,"score"), 3).localOrShuffleGrouping("strategyScoringBolt", "score_stream");
         //topologyBuilder.setBolt("memberPublishBolt", new MemberPublishBolt(RedisConnection.getServers()[0], 6379,"member"), 3).localOrShuffleGrouping("strategyScoringBolt", "member_stream");
