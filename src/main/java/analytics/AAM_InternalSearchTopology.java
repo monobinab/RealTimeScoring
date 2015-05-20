3bd91c6c4cdd882e34279057068dd8b1a5047eec
spannal <spannal@ushoflt323507.kih.kmart.com>
2015-05-20 15:57:58 -0500

    Internal Search modified with correct bolt listeners

--- src/main/java/analytics/AAM_InternalSearchTopology.java
+++ src/main/java/analytics/AAM_InternalSearchTopology.java
@@ -45,7 +45,7 @@ public static void main(String[] args) {
 		topologyBuilder.setBolt("strategy_bolt", new StrategyScoringBolt(System.getProperty(MongoNameConstants.IS_PROD)), 10)
 				.shuffleGrouping("ParsingBoltAAM_InternalSearch");
 		if(System.getProperty(MongoNameConstants.IS_PROD).equals("PROD")){
-			topologyBuilder.setBolt("flumeLoggingBolt", new FlumeRPCBolt(System.getProperty(MongoNameConstants.IS_PROD)), 1).shuffleGrouping("strategyScoringBolt", "score_stream");
+			topologyBuilder.setBolt("flumeLoggingBolt", new FlumeRPCBolt(System.getProperty(MongoNameConstants.IS_PROD)), 1).shuffleGrouping("strategy_bolt", "score_stream");
 		}
 		
 		
