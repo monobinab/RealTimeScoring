53066c2c6a8a2c5d6520efc9be696a1f3ae291fe
Christopher Barton <Chris.Barton@searshc.com>
2015-06-17 12:03:48 -0500

    Update SignalSpout.py

--- src/main/resources/SignalSpout.py
+++ src/main/resources/SignalSpout.py
@@ -7,7 +7,7 @@ import ast
 class SignalSpoutBase(storm.Spout):
 	y = requests.get('http://semantictec.com/message/consume?topic=user.activities.signal&size=100&consumerGroup=analytics&timeout=10')
 	def process(self):
-		for element in json.loads(self.y.content):
+		for element in json.loads(y.content):
 			try:
 				values = ast.literal_eval(element['value'])
 				storm.emit([values['channel'],values['products'],values['signalTime'],values['user']['sywrId'],values['user']['uuid']])
