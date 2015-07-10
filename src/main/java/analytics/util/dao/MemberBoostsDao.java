090135fba2a07fc7c4c6b3423a0b2019b45f09e2
Karththikka Muthukuri <kmuthuk@SYW00490GU13053.kih.kmart.com>
2015-07-10 16:27:40 -0500

    new MemberBoostsDao was created for every persist, not changed to prepare method

--- src/main/java/analytics/util/dao/MemberBoostsDao.java
+++ src/main/java/analytics/util/dao/MemberBoostsDao.java
@@ -1,10 +1,8 @@
 
 package analytics.util.dao;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
