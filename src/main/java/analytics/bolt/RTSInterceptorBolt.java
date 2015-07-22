3fc974d9ad3249c2105513f88c16dbefd38f4c46
unknown <pnair0@ACE00490GU1413A.kih.kmart.com>
2015-07-22 05:46:59 -0500

    Added the logger statement

--- src/main/java/analytics/bolt/RTSInterceptorBolt.java
+++ src/main/java/analytics/bolt/RTSInterceptorBolt.java
@@ -40,6 +40,7 @@ public class RTSInterceptorBolt extends EnvironmentBolt {
 		listToEmit.add(input.getValue(0));
 		//A message ID is added as in REDIS Spout
 		listToEmit.add(new Double(Math.random()).toString());
+		LOGGER.info("Emitting Record in RTSInterceptorBolt "+input.getValue(0) );
 		this.outputCollector.emit(listToEmit);
 		outputCollector.ack(input);
 	}
