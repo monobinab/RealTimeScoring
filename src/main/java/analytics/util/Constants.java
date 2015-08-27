e2bb1824838875ad55b843b38dbb122e4497d61c
pnair0 <pnair0@SYW00490EU17002.kih.kmart.com>
2015-08-27 16:07:28 -0500

    aam feed path updated to fetch from prod machines

--- src/main/java/analytics/util/Constants.java
+++ src/main/java/analytics/util/Constants.java
@@ -28,10 +28,10 @@ public class Constants {
 	//public static final String AAM_INTERNAL_SEARCH_PATH="/smith/adobe/rts/out/is_grouped_search";
 	//public static final String AAM_BROWSER_PATH = "/smith/adobe/rts/out/products_grouping";
 	
-	public static final String AAM_TRAITS_PATH="/smith/adobe/rts/out/traits";
+	public static final String AAM_TRAITS_PATH="/warehouse/aam/prod/rts/out/traits";
 	//public static final String AAM_BROWSER_PATH="/smith/adobe/rts/out/products";
 	public static final String AAM_INTERNAL_SEARCH_PATH="/warehouse/aam/prod/rts/out/is_grouped_search";
-	public static final String AAM_BROWSER_PATH = "/smith/adobe/rts/out/products_grouping";
+	public static final String AAM_BROWSER_PATH = "/warehouse/aam/prod/rts/out/products_grouping";
 	public static final String KERBEROSE_KEYTAB_PATH = "/appl/storm/keytabs/prod_aamadmin.keytab";
 	public static final String KERBEROSE_CONF_PATH = "/home/auto/spannal/krb5.conf";
 	//public static final String AAM_INTERNAL_SEARCH_PATH="/smith/adobe/rts/out/is";
