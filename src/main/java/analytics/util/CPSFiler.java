87baffdf90531442462821dd24c8eeb866ab549d
Jyothsna Palwai <jyothsna.palwai@searshc.com>
2015-09-21 12:48:25 -0500

    Fixed the IndexOutOFBoundException

--- src/main/java/analytics/util/CPSFiler.java
+++ src/main/java/analytics/util/CPSFiler.java
@@ -87,7 +87,9 @@ public class CPSFiler {
 					compareAndDelete(lyl_id_no,emailPackagesToBeSent, currentPackages);
 					
 					//Rule - top 5 % occasions are not queued
-					emailPackagesToBeSent = filterTop5PercentOccasions(emailPackagesToBeSent, inProgressPackage);
+					if(emailPackagesToBeSent != null && emailPackagesToBeSent.size() > 0){
+						emailPackagesToBeSent = filterTop5PercentOccasions(emailPackagesToBeSent, inProgressPackage);
+					}
 					
 					if(emailPackagesToBeSent != null && emailPackagesToBeSent.size() > 0){
 						return this.decideSendDates(emailPackagesToBeSent,inProgressPackage);
@@ -121,7 +123,7 @@ public class CPSFiler {
 				}
 			}
 			
-			if(emailPackagesToBeSent != null && emailPackagesToBeSent.size() > 0 && inProgressActive){
+			if( inProgressActive){
 				filteredList = removeTop5PercentOccasion(emailPackagesToBeSent);				
 			}	
 			else{
