f17f1a19b07bfa8ae67802472778ff9f40aee585
pnair0 <pnair0@SYW00490EU17002.kih.kmart.com>
2015-09-24 14:20:49 -0500

    set the number of tasks to 20 for parsing bolt...

--- src/main/java/analytics/AAM_InternalSearchTopology.java
+++ src/main/java/analytics/AAM_InternalSearchTopology.java
@@ -40,7 +40,7 @@ public static void main(String[] args) {
 		
 		//Sree. Spout that wakes up every 5 mins and process the Traits
 		topologyBuilder.setSpout("internalSearchSpout", new WebHDFSSpout(servers[1], TopicConstants.PORT, Constants.AAM_INTERNAL_SEARCH_PATH, "aamInternalSearch"), 1);
-		topologyBuilder.setBolt("ParsingBoltAAM_InternalSearch",new ParsingBoltAAM_InternalSearch(System.getProperty(MongoNameConstants.IS_PROD), source),10).shuffleGrouping("internalSearchSpout");
+		topologyBuilder.setBolt("ParsingBoltAAM_InternalSearch",new ParsingBoltAAM_InternalSearch(System.getProperty(MongoNameConstants.IS_PROD), source),10).shuffleGrouping("internalSearchSpout").setNumTasks(20);
 
 		topologyBuilder.setBolt("strategy_bolt", new StrategyScoringBolt(System.getProperty(MongoNameConstants.IS_PROD)), 10)
 				.shuffleGrouping("ParsingBoltAAM_InternalSearch");
