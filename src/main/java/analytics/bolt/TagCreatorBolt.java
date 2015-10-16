c9618f360d163b34e3d7fdb763a3b718ba2863ca
pnair0 <pnair0@SYW00490EU17002.kih.kmart.com>
2015-10-16 16:34:55 -0500

    lyl_id_no.toString() corrected to lyl_id_no.getAsString()

--- src/main/java/analytics/bolt/TagCreatorBolt.java
+++ src/main/java/analytics/bolt/TagCreatorBolt.java
@@ -84,7 +84,7 @@ public class TagCreatorBolt extends EnvironmentBolt  {
 				LOGGER.info("Input from TagCreatorBolt :" + jsonElement.toString());
 				JsonElement lyl_id_no = jsonElement.getAsJsonObject().get("memberId");
 				
-				BigInteger loyaltyID =  new BigInteger(lyl_id_no.toString());
+				BigInteger loyaltyID =  new BigInteger(lyl_id_no.getAsString());
 				//if (! (loyaltyID.compareTo(startLoyalty) != -1  && loyaltyID.compareTo(lastLoyalty) != 1) ){
 				
 				if (loyaltyID.compareTo(startLoyalty) == -1  || loyaltyID.compareTo(lastLoyalty) == 1) {
