605792ae4abbfc3518801818a7a809a747d23fec
Mishra <amishr8@ushofml306690.kih.kmart.com>
2015-12-02 14:59:15 -0600

    Change cron scheduler cache refresh time to 2:00 am everyday

--- src/main/java/analytics/util/dao/caching/RTSQuartzScheduler.java
+++ src/main/java/analytics/util/dao/caching/RTSQuartzScheduler.java
@@ -15,13 +15,14 @@ public class RTSQuartzScheduler {
 		
 		JobDetail job = JobBuilder.newJob(CacheRefreshJob.class).withIdentity("cacheRefreshJob", "group1").build();
 		if(job != null){
-			Trigger trigger = TriggerBuilder.newTrigger().
-					withIdentity("cachRefreshTrigger", "group1").
-					withSchedule(CronScheduleBuilder.cronSchedule("0/60 * * * * ?")).build();
 			/**
 			Trigger trigger = TriggerBuilder.newTrigger().
 					withIdentity("cachRefreshTrigger", "group1").
-					withSchedule(CronScheduleBuilder.cronSchedule("0 0 14 * * ?")).build();*/
+					withSchedule(CronScheduleBuilder.cronSchedule("0/60 * * * * ?")).build();*/
+			
+			Trigger trigger = TriggerBuilder.newTrigger().
+					withIdentity("cachRefreshTrigger", "group1").
+					withSchedule(CronScheduleBuilder.cronSchedule("0 0 14 * * ?")).build();
 			//schedule it
 	    	Scheduler scheduler = new StdSchedulerFactory().getScheduler();
 	    	scheduler.start();
