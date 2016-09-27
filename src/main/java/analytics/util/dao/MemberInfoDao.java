4bd328a495713280c836f55b5493c0c81ba738fb
Nair <pnair0@searshc.com>
2016-09-27 16:48:27 -0500

    Modified memberinfo DAO to look up srse for sears optin

--- src/main/java/analytics/util/dao/MemberInfoDao.java
+++ src/main/java/analytics/util/dao/MemberInfoDao.java
@@ -30,9 +30,11 @@ public class MemberInfoDao extends AbstractDao {
 		info.setEid(obj.get(MongoNameConstants.E_ID)==null?null:obj.get(MongoNameConstants.E_ID).toString());
 		info.setEmailOptIn(obj.get(MongoNameConstants.EMAIL_OPT_IN)==null?null:obj.get(MongoNameConstants.EMAIL_OPT_IN).toString());
 		info.setState(obj.get(MongoNameConstants.STATE)==null?null:obj.get(MongoNameConstants.STATE).toString());
-		info.setSrs_opt_in(obj.get(MongoNameConstants.SEARS_OPT_IN)==null?null:obj.get(MongoNameConstants.SEARS_OPT_IN).toString());
+		info.setSrs_opt_in(obj.get(MongoNameConstants.SRS_OPT_IN)==null?null:obj.get(MongoNameConstants.SRS_OPT_IN).toString());
+		//info.setSrs_opt_in(obj.get(MongoNameConstants.SEARS_OPT_IN)==null?null:obj.get(MongoNameConstants.SEARS_OPT_IN).toString());
 		info.setKmt_opt_in(obj.get(MongoNameConstants.KMART_OPT_IN)==null?null:obj.get(MongoNameConstants.KMART_OPT_IN).toString());
 		info.setSyw_opt_in(obj.get(MongoNameConstants.SYW_OPT_IN)==null?null:obj.get(MongoNameConstants.SYW_OPT_IN).toString());
+		
 		info.setText_opt_in(obj.get(MongoNameConstants.TEXT_OPT_IN)==null?null:obj.get(MongoNameConstants.TEXT_OPT_IN).toString());
 		info.setSrs_zip(obj.get(MongoNameConstants.SRS_ZIP)==null?null:obj.get(MongoNameConstants.SRS_ZIP).toString());
 		info.setKmt_zip(obj.get(MongoNameConstants.KMT_ZIP)==null?null:obj.get(MongoNameConstants.KMT_ZIP).toString());
